8ea25b0d42ebff939e5e65f8a0fc383ad6b192f2
tadej <tadej@mail.com>
2019-02-03 15:26:05 +0100

    find common elements in 2 arrays

--- find-common-elements-of-two-arrays.js
+++ find-common-elements-of-two-arrays.js
@@ -1,13 +1,15 @@
+/*
+* find common elements in two arrays - array intersection
+*/
 function findCommonEl(arr1, arr2) {
   let el,
      exists = {},
      common = [];
 
   /*
-  loop throught first and second array and compare
-  if element from arr1 and arr2 are equal
-  and element not yet in the exist object
-  -> push in the common element array
+  * loop throught first and second array and compare
+  * if element from arr1 and arr2 are equal and element not yet in the exist object
+  * push in the common element array
   */
   for (let i of arr1) {
     el = i;
@@ -22,7 +24,3 @@ function findCommonEl(arr1, arr2) {
   return common;
 }
 
-var para = document.querySelector('p');
-var example1 = [1, 2, 4, 4, 6, 8, 11];
-var example2 = [3, 5, 7, 8, 9, 10, 11, 12, 13];
-// para.innerHTML = findCommonEl(example1, example2);
