c81e31ce37d2764963998d56f8c76371b5dd1258
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-17 20:38:27 +0200

    two sum algorithm

--- two-sum.js
+++ two-sum.js
@@ -1,13 +1,20 @@
-/*
-* two sum algorithm
-* given an array of integers, return indices of the two numbers such that they add up to a specific target
-*/
+/**
+ * Two sum algorithm
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target
+ * 
+ * @param {array} arr 
+ * @param {number} target 
+ * 
+ * @returns {array} indices of array elements
+ */
 function twoSum (arr, target) {
     const prevVals = {};
     const n = arr.length;
 
-    // for each number in array get diff for target first
-    // if this value exists in object return both indexes
+    /*
+    * For each number in array get diff for target first
+    * If this value exists in object return both indexes
+    */
     for(let i = 0; i < n; i++) {
         const missingValue = target - arr[i];
         if(missingValue in prevVals) {
