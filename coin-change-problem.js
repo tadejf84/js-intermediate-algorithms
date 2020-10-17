50bdf6a632419ffe4d52a08ae05919639bf956f6
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-17 19:49:05 +0200

    calculate coin change algorithm

--- coin-change-problem.js
+++ coin-change-problem.js
@@ -7,28 +7,28 @@
  * @returns {array} change
  */
 function calcChange(cReturn, denominations) {
-	let change = [];
-	
-	// Loop through each denomination and calculate change
-	for (let den of denominations) {
+    let change = [];
 
-		let count = 0;
+    // Loop through each denomination and calculate change
+    for (let den of denominations) {
 
-		if(cReturn % den < cReturn) {
-			count = parseInt(cReturn / den);
-		} 
-				
-		// Decrease return
-		cReturn = cReturn - den * count;
-		
-		// Push denominations value to change array
-		const obj = { 
-			'denomination': den,
-			'count': count
-		};
+        let count = 0;
 
-		change.push(obj);
-	}
-	
-	return change;
+        if(cReturn % den < cReturn) {
+            count = parseInt(cReturn / den);
+        } 
+                
+        // Decrease return
+        cReturn = cReturn - den * count;
+        
+        // Push denominations value to change array
+        const obj = { 
+            'denomination': den,
+            'count': count
+        };
+
+        change.push(obj);
+    }
+
+    return change;
 }
