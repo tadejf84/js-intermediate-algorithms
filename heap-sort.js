41e355726d378f34973b9b9423e6cb0156089e21
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-17 20:11:57 +0200

    heap sort algorithm

--- heap-sort.js
+++ heap-sort.js
@@ -7,21 +7,21 @@
  * @returns {array} sorted array
  */
 function heapSort(arr) {
-	const len = arr.length;
-	let last = len - 1;
+    const len = arr.length;
+    let last = len - 1;
 
-	// 1. Build a max heap from the array
-	buildMaxHeap(arr, len);
+    // 1. Build a max heap from the array
+    buildMaxHeap(arr, len);
 
-	// 2. Continue heap sorting until we have at least one element
-	while(last > 0) {
-		swap(arr, 0, last);
-		heapify(arr, 0, last);
-		last--;
-	}
+    // 2. Continue heap sorting until we have at least one element
+    while(last > 0) {
+        swap(arr, 0, last);
+        heapify(arr, 0, last);
+        last--;
+    }
 
-	// 3. Return sorted array
-	return arr;
+    // 3. Return sorted array
+    return arr;
 }
 
 
@@ -34,14 +34,14 @@ function heapSort(arr) {
  */
 function buildMaxHeap(arr, len) {
 
-	// Split in a binary tree
-	let i = Math.floor(len / 2 - 1);
+    // Split in a binary tree
+    let i = Math.floor(len / 2 - 1);
 
-	// Build a max heap
-	while (i >= 0) {
-		heapify(arr, i, len);
-		i--;
-	}
+    // Build a max heap
+    while (i >= 0) {
+        heapify(arr, i, len);
+        i--;
+    }
 
 }
 
@@ -57,23 +57,23 @@ function buildMaxHeap(arr, len) {
  * @param {number} len 
  */
 function heapify(heap, i, len) {
-	let root, 
-		left, 
-		right;
+    let root, 
+        left, 
+        right;
 
-	while(i < len) {
-		root = i;
+    while(i < len) {
+        root = i;
 
-		left = 2 * i + 1; 	// Left child node
-		right = 2 * i + 2; 	// Right child node
+        left = 2 * i + 1; 	// Left child node
+        right = 2 * i + 2; 	// Right child node
 
-		if (left < len && heap[left] > heap[root]) root = left;
-		if (right < len && heap[right] > heap[root]) root = right;
-		if (root === i) return;
+        if (left < len && heap[left] > heap[root]) root = left;
+        if (right < len && heap[right] > heap[root]) root = right;
+        if (root === i) return;
 
-		swap(heap, i, root);
-		i = root;
-	}
+        swap(heap, i, root);
+        i = root;
+    }
 
 }
 
@@ -86,8 +86,8 @@ function heapify(heap, i, len) {
  * @param {number} j 
  */
 function swap(arr, i, j) {
-	let temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
