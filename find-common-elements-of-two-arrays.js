46a13bcc0f7eb235549e4dc2a824f09ccd2dcba8
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-17 20:05:47 +0200

    array intersection algorithm

--- find-common-elements-of-two-arrays.js
+++ find-common-elements-of-two-arrays.js
@@ -1,26 +1,28 @@
-/*
-* find common elements in two arrays - array intersection
-*/
+/**
+ * Find common elements in two arrays - array intersection
+ * 
+ * @param {array} arr1 
+ * @param {array} arr2 
+ * 
+ * @returns {array} array intersection
+ */
 function findCommonEl(arr1, arr2) {
-  let el,
-     exists = {},
-     common = [];
+    let el,
+        exists = {},
+        common = [];
 
-  /*
-  * loop throught first and second array and compare
-  * if element from arr1 and arr2 are equal and element not yet in the exist object
-  * push in the common element array
-  */
-  for (let i of arr1) {
-    el = i;
-    for (let j of arr2) {
-      if (el === j && !exists[j]) {
-        common.push(el);
-        exists[j] = true;
-      }
+    for (let i of arr1) {
+        el = i;
+
+        for (let j of arr2) {
+            if (el === j && !exists[j]) {
+                common.push(el);
+                exists[j] = true;
+            }
+        }
+
     }
-  }
 
-  return common;
+    return common;
 }
 
