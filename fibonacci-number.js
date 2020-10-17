e1539b7e800ca7515d73ad7c0ed988919f95e02b
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-17 20:02:44 +0200

    fibonacci number algorithm

--- fibonacci-number.js
+++ fibonacci-number.js
@@ -1,60 +1,75 @@
-/*
-* fibonacci number is the sum of the previous two numbers in the fibonacci sequence
-* fibonacci with a loop
-*/
+/**
+ * Fibonacci number is the sum of the previous two numbers in the fibonacci sequence
+ * 
+ * @param {number} n
+ * 
+ * @returns {number} fibonacci number
+ */
 function fibonacci1(n) {
-  let prev1, prev2, fib;
+    let prev1, 
+        prev2, 
+        fib;
 
-  for(let i = 0; i <= n; i++) {
-    if (i === 0) {
-      prev1 = 0;
-      fib = prev1;
-    } else if (i === 1) {
-      prev2 = 1;
-      fib = prev2;
-    } else {
-      fib = prev1 + prev2;
-      prev1 = prev2;
-      prev2 = fib;
+    for(let i = 0; i <= n; i++) {
+        if (i === 0) {
+            prev1 = 0;
+            fib = prev1;
+        } else if (i === 1) {
+            prev2 = 1;
+            fib = prev2;
+        } else {
+            fib = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = fib;
+        }
     }
-  }
-  return fib;
+
+    return fib;
 }
 
 
-/*
-* fibonacci recursive
-*/
+/**
+ * Fibonacci recursive implementation
+ * 
+ * @param {number} n 
+ * 
+ * @returns {number} fibonacci number
+ */
 function fibonacci2(n) {
-	if (n < 1) {
-    return 0;
-  } else if (n <= 2) {
-    return 1;
-  } else {
-    return fibonacci2(n - 1) + fibonacci2(n - 2);
-  }
+    if (n < 1) {
+        return 0;
+    } else if (n <= 2) {
+        return 1;
+    } else {
+        return fibonacci2(n - 1) + fibonacci2(n - 2);
+    }
 }
 
 
-/*
-* fibonacci with caching - much better performance
-*/
 let cache = {};
+
+/**
+ * Fibonacci with caching - much better performance
+ * 
+ * @param {number} n 
+ * 
+ * @returns {number} fibonacci number
+ */
 function fibonacci3(n) {
-  let output;
+    let output;
 
-  if (n < 1) {
-    return 0;
-  } else if (n <= 2) {
-    return 1;
-  } else if (cache[n] != null) {
-    return cache[n];
-  } else {
-    output = fibonacci3(n - 1) + fibonacci3(n - 2);
-  }
+    if (n < 1) {
+        return 0;
+    } else if (n <= 2) {
+        return 1;
+    } else if (cache[n] != null) {
+        return cache[n];
+    } else {
+        output = fibonacci3(n - 1) + fibonacci3(n - 2);
+    }
 
-  // caching
-  cache[n] = output;
+    // Caching
+    cache[n] = output;
 
-  return output;
+    return output;
 }
